bca4d0443b98261247b319a9f0e843a64e32deac
Princy <rafamatanantsoa10@gmail.com>
2024-01-31 15:26:56 +0300

    first commit

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-//on init
+// on init
 app.get('/', (req, res) => {
     res.send('Hello')
 })
