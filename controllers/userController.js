40af9d96892228b47c623713be0e4a2ba0e09d70
Princy10 <rafamatanantsoa10@gmail.com>
2024-02-16 14:52:40 +0300

    update profile a continuer

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,11 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
-const Individu = require('../models/individu');
-const asyncHandler = require('express-async-handler');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const Individu = require("../models/individu");
+const asyncHandler = require("express-async-handler");
 
 const getEmployer = asyncHandler(async (req, res) => {
-  const users = await User.find({ role: 'employer' });
+  const users = await User.find({ role: "employer" });
 
   if (!users || users.length === 0) {
     res.status(404);
@@ -16,10 +16,63 @@ const getEmployer = asyncHandler(async (req, res) => {
     const individu = await Individu.findById(user.id_individu);
     return individu;
   });
-  
+
   const individus = await Promise.all(individusPromises);
 
   res.json(individus);
 });
 
-module.exports = { getEmployer, getUserById };
+const updateUser = asyncHandler(async (req, res) => {
+  try {
+    const {
+      id,
+      username,
+      currentPassword,
+      newPassword,
+      nom,
+      prenom,
+      mail,
+      date_naissance,
+      cin,
+      adresse,
+      contact
+    } = req.body;
+
+    const user = await User.findById(id).populate('id_individu');
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    if (currentPassword) {
+      const isPasswordMatch = await user.matchPassword(currentPassword);
+      if (!isPasswordMatch) {
+        return res.status(400).json({ error: 'Current password is incorrect' });
+      }
+    }
+
+    user.username = username;
+    if (newPassword) {
+      user.password = await bcrypt.hash(newPassword, 10);
+    }
+
+    ////manomboka et le tsy tafiditra
+    user.id_individu.nom = nom;
+    user.id_individu.prenom = prenom;
+    user.id_individu.mail = mail;
+    user.id_individu.date_naissance = date_naissance;
+    user.id_individu.cin = cin;
+    user.id_individu.adresse = adresse;
+    user.id_individu.contact = contact;
+
+
+    /// ty no mampiditra azy fa le individu tsy tafiditra
+    await user.save();
+
+    res.status(200).json({ message: 'User updated successfully' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = { getEmployer, updateUser };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getEmployer } = require('../controllers/userController');
+const { getEmployer, updateUser } = require('../controllers/userController');
 
 const router = express.Router();
 
 router.get('/list/employer', getEmployer);
+router.put('/update', updateUser);
 
 module.exports = router;
