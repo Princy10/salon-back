c78a0f9885de4b8b213c099a1f534d42cbfd0de2
Nampiana <89507479+Nampiana@users.noreply.github.com>
2024-02-17 11:12:23 +0100

    RDV employer

--- controllers/priseRdvController.js
+++ controllers/priseRdvController.js
@@ -1,6 +1,9 @@
 const Rdv = require('../models/rdv');
 const Rdv_service = require('../models/rdv_service');
+const User = require("../models/user");
+const Individu = require("../models/individu");
 const asyncHandler = require('express-async-handler');
+const nodemailer = require("nodemailer");
 
 const insererRdvEtServices = asyncHandler(async (req, res) => {
   try {
@@ -30,6 +33,143 @@ const insererRdvEtServices = asyncHandler(async (req, res) => {
   }
 });
 
+//RDV  EMPLOYER
+const getRdvEmplByID = asyncHandler(async(req, res) => {
+  try {
+      const {id} = req.params;
+      const rdvs = await Rdv.find({ id_individu_empl: id }).populate('id_individu_client', 'nom prenom').populate('id_individu_empl', 'nom prenom');
+      const rdvsWithServices = [];
+
+    for (const rdv of rdvs) {
+      const rdvServices = await Rdv_service.find({ id_rdv: rdv._id }).populate('id_service');
+      const rdvWithServices = { ...rdv._doc, services: rdvServices.map(rdvService => rdvService.id_service) };
+      rdvsWithServices.push(rdvWithServices);
+    }
+      res.status(200).json(rdvsWithServices);
+  } catch (error) {
+      res.status(500);
+      throw new error(error.message);
+  }
+})
+
+const getRdvByID = asyncHandler(async(req, res) => {
+  try {
+    const {id} = req.params;
+    const rdv = await Rdv.findById(id)
+      .populate('id_individu_client', 'nom prenom')
+      .populate('id_individu_empl', 'nom prenom');
+
+    if (!rdv) {
+      res.status(404);
+      throw new Error('Rendez-vous not found');
+    }
+
+    const rdvServices = await Rdv_service.find({ id_rdv: rdv._id }).populate('id_service');
+
+    const response = {
+      _id: rdv._id,
+      id_individu_client: rdv.id_individu_client,
+      id_individu_empl: rdv.id_individu_empl,
+      date_heure: rdv.date_heure,
+      etat: rdv.etat,
+      services: rdvServices.map(rdvService => rdvService.id_service),
+    };
+
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500);
+    throw new Error(error.message);
+  }
+});
+
+const etatRdvValider = asyncHandler(async(req, res) => {
+  try {
+      const {id} = req.params;
+      const rdv = await Rdv.findByIdAndUpdate(id, { etat: "Valider" }, { new: true });
+
+      if(!rdv){
+          res.status(404);
+          throw new Error(`Cannot find or update the rendez-vous with ID ${id}`);
+      }
+
+      const user = await User.findOne({ id_individu: rdv.id_individu_client });
+
+      const individu = await Individu.findById(rdv.id_individu_client);
+
+      if (!user || !individu) {
+        res.status(404);
+        throw new Error(`Cannot find user or individu associated with the rendez-vous with ID ${id}`);
+    }
+
+      const message = `Bonjour ,\nVotre rendez-vous est valider à ${rdv.date_heure}`;
+      sendEmail(individu.mail, "Rendez-vous valider", message);
+      res.status(200).json(rdv);
+      
+  } catch (error) {
+      res.status(500);
+      throw new Error(error.message);
+  }
+});
+
+const etatRdvRefuser = asyncHandler(async(req, res) => {
+  try {
+      const {id} = req.params;
+      const rdv = await Rdv.findByIdAndUpdate(id, { etat: "Refuser" }, { new: true });
+
+      if(!rdv){
+          res.status(404);
+          throw new Error(`Cannot find or update the rendez-vous with ID ${id}`);
+      }
+
+      const user = await User.findOne({ id_individu: rdv.id_individu_client });
+
+      const individu = await Individu.findById(rdv.id_individu_client);
+
+      if (!user || !individu) {
+        res.status(404);
+        throw new Error(`Cannot find user or individu associated with the rendez-vous with ID ${id}`);
+    }
+
+      const message = `Bonjour ,\nVotre rendez-vous le ${rdv.date_heure} est réfuser parceque la journée est déja remplie`;
+      sendEmail(individu.mail, "Rendez-vous valider", message);
+      res.status(200).json(rdv);
+      
+  } catch (error) {
+      res.status(500);
+      throw new Error(error.message);
+  }
+});
+
+const sendEmail = (to, subject, text) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "nampiana.fanamperana@gmail.com",
+      pass: "vagp hfud nbfg buwu",
+    },
+  });
+
+  const mailOptions = {
+    from: "nampiana.fanamperana@gmail.com",
+    to,
+    subject,
+    text,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+};
+
+
 module.exports = {
-  insererRdvEtServices
+  insererRdvEtServices,
+  getRdvEmplByID,
+  getRdvByID,
+  etatRdvValider,
+  etatRdvRefuser
 };
--- routes/priseRdvRoute.js
+++ routes/priseRdvRoute.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { insererRdvEtServices } = require('../controllers/priseRdvController');
+const { insererRdvEtServices, getRdvEmplByID, getRdvByID, etatRdvValider, etatRdvRefuser} = require('../controllers/priseRdvController');
 
 const router = express.Router();
 
 router.post('/create', insererRdvEtServices);
+router.get('/list/:id', getRdvEmplByID);
+router.get('/listRdvById/:id', getRdvByID);
+router.put('/update_valider/:id', etatRdvValider);
+router.put('/update_refuser/:id', etatRdvRefuser);
 
 module.exports = router;
